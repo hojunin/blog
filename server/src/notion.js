5dfcc7b42d31b6498da1866ea417cf5603fe54d6
hojunin <dlsghwns@naver.com>
2022-04-22 00:08:56 +0900

    쿼리파라미터로 실데이터 핸들링 기능 추가

--- server/src/notion.js
+++ server/src/notion.js
@@ -1,54 +1,77 @@
-import { Client, LogLevel } from "@notionhq/client";
-import dotenv from "dotenv";
+import { Client, LogLevel } from '@notionhq/client';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const notion = new Client({
-  auth: process.env.NOTION_ACCESS_KEY,
-  logLevel: LogLevel.DEBUG,
+    auth: process.env.NOTION_ACCESS_KEY,
+    logLevel: LogLevel.DEBUG,
 });
 
 export async function getDB() {
-  try {
-    const data = await notion.databases.query({
-      database_id: process.env.NOTION_DATABASE_KEY,
-    });
+    try {
+        const data = await notion.databases.query({
+            database_id: process.env.NOTION_DATABASE_KEY,
+        });
 
-    return data;
-  } catch (error) {
-    console.error(error.body);
-  }
+        return data;
+    } catch (error) {
+        console.error(error.body);
+    }
 }
 
 export async function getPage(page_id) {
-  try {
-    const page = await getBlocks(page_id);
-    let pagePromises = [];
-    for (let result of page.results) {
-      if (result.has_children) {
-        const childrenBlocks = await getBlocks(result.id);
-        result.childrens = childrenBlocks;
-      }
-      pagePromises.push(result);
+    try {
+        const page = await getBlocks(page_id);
+        let pagePromises = [];
+        for (let result of page.results) {
+            if (result.has_children) {
+                const childrenBlocks = await getBlocks(result.id);
+                result.childrens = childrenBlocks;
+            }
+            pagePromises.push(result);
+        }
+
+        const data = await Promise.all(pagePromises);
+
+        return data;
+    } catch (err) {
+        throw new Error(err);
     }
-
-    const data = await Promise.all(pagePromises);
-
-    return data;
-  } catch (err) {
-    throw new Error(err);
-  }
 }
 
 export async function getBlocks(block_id) {
-  try {
-    const blocks = await notion.blocks.children.list({
-      block_id,
-      page_size: 50,
-    });
+    try {
+        const blocks = await notion.blocks.children.list({
+            block_id,
+            page_size: 50,
+        });
 
-    return blocks;
-  } catch (err) {
-    console.error(err);
-    throw new Error(err);
-  }
+        return blocks;
+    } catch (err) {
+        console.error(err);
+        throw new Error(err);
+    }
+}
+
+export async function updateStatus(page_id, status) {
+    try {
+        await notion.pages.update({
+            page_id,
+            properties: {
+                status,
+            },
+        });
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+export async function searchPost(keyword) {
+    try {
+        return await notion.search({
+            query: keyword,
+        });
+    } catch (error) {
+        throw new Error();
+    }
 }
